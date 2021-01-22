cbd0842ff010681aeee8e1db64c3bb96426b9c2f
nairol <73656754+nairol203@users.noreply.github.com>
2021-01-23 00:20:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.Client()
+const client = new Discord.Client();
+
+const chatting = require('./chatting');
 
 client.on('ready', () => {
-    client.user.setActivity(`dir`, { type : 'LISTENING' });
-})
+	client.user.setActivity(`dir`, { type : 'LISTENING' });
+	chatting(client)
+});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
